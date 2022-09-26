7371777a6f6d7be4bb58a9e5f5f8592c9acfcd56
moscocloud <skochchad97@gmail.com>
2022-09-26 10:52:06 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,22 @@
+const userInput = document.getElementById("userInput");
+var expression = '';
+
+function press(num){
+    expression += num;
+    userInput.value = expression;
+}
+
+function equal(){
+    if (!isNaN(parseFloat(userInput.value)) && isFinite(userInput.value)) {
+        
+    } else if (userInput.value == 0) {
+    } else {
+        userInput.value = eval(expression);
+    }
+    }
+
+
+function erase(){
+    expression=''
+    userInput.value = expression
+}
